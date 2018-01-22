c2705e7fa4625bc2b30cd5c9f84f29d3c1491048
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-01-22 23:28:27 +0100

    Create working example of reading JSON from URL

--- src/main/agh/cs/lab/termly/WebApiClient.java
+++ src/main/agh/cs/lab/termly/WebApiClient.java
@@ -1,16 +1,40 @@
 package agh.cs.lab.termly;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class WebApiClient {
 
-    
+    class HistoryEntry {
+        private String fromDateTime;
+        private String tillDateTime;
+
+        @Override
+        public String toString() {
+            return fromDateTime +  " - " + tillDateTime;
+        }
+    }
+
+    class Response {
+        public HistoryEntry history[];
+
+        @Override
+        public String toString() {
+            return Arrays.stream(history).map(HistoryEntry::toString).collect
+                    (Collectors.joining("\n"));
+        }
+    }
+
 
     private final String apiUrl;
 
@@ -31,7 +55,22 @@ public class WebApiClient {
             BufferedReader reader = new BufferedReader(new InputStreamReader
                     (conn.getInputStream()));
 
-            reader.lines().forEach(System.out::println);
+//            reader.lines().forEach(System.out::println);
+
+//            String content = reader.lines().collect(Collectors.joining("\n"));
+
+            Gson gson = new Gson();
+            Response result = gson.fromJson(reader, Response.class);
+
+            System.out.println(result);
+
+//            JsonReader jsonReader = new JsonReader(new InputStreamReader(conn
+//                    .getInputStream()));
+
+//            jsonReader.beginObject();
+//            jsonReader.has
+
+
 
         } catch (Exception e) {
             // TODO
